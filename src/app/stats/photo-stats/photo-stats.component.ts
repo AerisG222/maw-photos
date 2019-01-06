691ef284cf76c651a59e1468c469c97ea07a3f46
Mike Morano <mmorano@mikeandwan.us>
2019-01-06 08:30:57 -0500

    initial stats charting support

--- src/app/stats/photo-stats/photo-stats.component.ts
+++ src/app/stats/photo-stats/photo-stats.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { _appIdRandomProviderFactory } from '@angular/core/src/application_tokens';
+import { BehaviorSubject } from 'rxjs';
+import { colorSets } from '@swimlane/ngx-charts/release/utils';
 
 import { PHOTO_API_SERVICE, IPhotoApiService } from 'src/app/services/iphoto-api.service';
+import { IYearStats } from 'src/app/models/iyear-stats.model';
 
 @Component({
     selector: 'app-photo-stats',
@@ -9,11 +12,48 @@ import { PHOTO_API_SERVICE, IPhotoApiService } from 'src/app/services/iphoto-api
     styleUrls: ['./photo-stats.component.scss']
 })
 export class PhotoStatsComponent implements OnInit {
+    private _stats: IYearStats[];
+
+    colorScheme = colorSets.find(s => s.name === 'cool');
+    chartData$ = new BehaviorSubject<any>(null);
+
     constructor(@Inject(PHOTO_API_SERVICE) private _api: IPhotoApiService) {
 
     }
 
     ngOnInit() {
-        this._api.getPhotoStats().subscribe();
+        this._api
+            .getPhotoStats()
+            .subscribe(x => {
+                this._stats = x;
+                this.showYearsOverview();
+            });
+    }
+
+    onSelect(evt): void {
+        console.log(evt);
+    }
+
+    showYearsOverview(): void {
+        this.chartData$.next(
+            this._stats
+                .map(x => ({
+                    'name': x.year,
+                    'value': x.categoryStats
+                        .map(c => c.photoCount)
+                        .reduce((total, count) => count + total)
+                }))
+        );
+    }
+
+    showYearDetails(year: number): void {
+        this.chartData$.next(
+            this._stats
+                .filter(x => x.year === year)[0].categoryStats
+                .map(x => ({
+                    'name': x.name,
+                    'value': x.photoCount
+                }))
+        );
     }
 }
--- src/app/stats/stats.module.ts
+++ src/app/stats/stats.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { StatsRoutingModule } from './stats-routing.module';
 import { PhotoStatsComponent } from './photo-stats/photo-stats.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [PhotoStatsComponent],
   imports: [
     CommonModule,
+    NgxChartsModule,
     StatsRoutingModule
   ]
 })
