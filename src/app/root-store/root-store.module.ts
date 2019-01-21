66e0c9e031e4babd6c88b7272330bf86989b5c99
Mike Morano <mmorano@mikeandwan.us>
2019-01-21 15:53:32 -0500

    add ngrx freeze, but is it correct?

--- src/app/root-store/root-store.module.ts
+++ src/app/root-store/root-store.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, MetaReducer, State } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { storeFreeze } from 'ngrx-store-freeze';
 
 import { SettingsStoreModule } from './settings-store';
 import { environment } from '../../environments/environment';
 import { PhotoCategoryStoreModule } from './photo-category-store/photo-category-store.module';
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [storeFreeze] : [];
 
 @NgModule({
     declarations: [],
@@ -20,6 +23,8 @@ import { PhotoCategoryStoreModule } from './photo-category-store/photo-category-
         }),
         StoreModule.forRoot({
             router: routerReducer
+        }, {
+            metaReducers
         }),
         EffectsModule.forRoot([]),
         !environment.production ? StoreDevtoolsModule.instrument() : []
