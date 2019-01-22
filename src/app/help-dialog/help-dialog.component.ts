a00091ef46b2d590395c5ef9269b441a1d17c6bf
Mike Morano <mmorano@mikeandwan.us>
2019-01-22 09:50:01 -0500

    update local model naming per style guide

--- src/app/help-dialog/help-dialog.component.ts
+++ src/app/help-dialog/help-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
-import { IHelpInfo } from './ihelp-info.model';
+import { HelpInfo } from './help-info.model';
 
 @Component({
     selector: 'app-help-dialog',
@@ -11,7 +11,7 @@ import { IHelpInfo } from './ihelp-info.model';
 export class HelpDialogComponent {
     constructor(
         private _dialogRef: MatDialogRef<HelpDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) public info: IHelpInfo
+        @Inject(MAT_DIALOG_DATA) public info: HelpInfo
     ) {
 
     }
--- src/app/help-dialog/ihelp-info.model.ts
+++
@@ -1,3 +0,0 @@
-export interface IHelpInfo {
-    version: string;
-}
