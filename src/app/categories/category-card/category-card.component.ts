62c608d5b5117c548abc8e86d5cb42da2fa807d7
Mike Morano <mmorano@mikeandwan.us>
2019-01-24 14:50:17 -0500

    start to specify change detection strategy where we can

--- src/app/categories/category-card/category-card.component.ts
+++ src/app/categories/category-card/category-card.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 
 import { Category } from '../../core/models/category.model';
 
 @Component({
     selector: 'app-category-card',
     templateUrl: './category-card.component.html',
-    styleUrls: ['./category-card.component.scss']
+    styleUrls: ['./category-card.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CategoryCardComponent {
     @Input() category: Category;
--- src/app/categories/category-list/category-list.component.ts
+++ src/app/categories/category-list/category-list.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 
 import { Category } from '../../core/models/category.model';
 
 @Component({
     selector: 'app-category-list',
     templateUrl: './category-list.component.html',
-    styleUrls: ['./category-list.component.scss']
+    styleUrls: ['./category-list.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CategoryListComponent {
     @Input() categories: Category[];
