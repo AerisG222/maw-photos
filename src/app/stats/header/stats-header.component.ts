3ef39247ab72841f7693ce32d6e766e75b45c29c
Mike Morano <mmorano@mikeandwan.us>
2019-01-29 07:53:11 -0500

    onpush

--- src/app/stats/header/stats-header.component.ts
+++ src/app/stats/header/stats-header.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     selector: 'app-stats-header',
     templateUrl: './stats-header.component.html',
-    styleUrls: ['./stats-header.component.scss']
+    styleUrls: ['./stats-header.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StatsHeaderComponent {
     @Input() year: number;
