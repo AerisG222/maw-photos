4eec4250a4d48a5970bf8bfdc4ad825bf8f45b5a
Mike Morano <mmorano@mikeandwan.us>
2019-03-10 10:23:23 -0400

    build out more themes, and start to integrate across components

--- src/app/core/models/theme.model.ts
+++ src/app/core/models/theme.model.ts
@@ -1,10 +1,16 @@
 export class Theme {
     static readonly themeDark = new Theme('Dark', 'maw-dark-theme');
     static readonly themeLight = new Theme('Light', 'maw-light-theme');
+    static readonly themeMulledWine = new Theme('Mulled Wine', 'maw-mulled-wine-theme');
+    static readonly themeOcean = new Theme('Ocean', 'maw-ocean-theme');
+    static readonly themePaleNight = new Theme('Pale Night', 'maw-pale-night-theme');
 
     static readonly allThemes = [
         Theme.themeDark,
-        Theme.themeLight
+        Theme.themeLight,
+        Theme.themeMulledWine,
+        Theme.themeOcean,
+        Theme.themePaleNight
     ];
 
     readonly name: string;
@@ -21,6 +27,12 @@ export class Theme {
                 return Theme.themeDark;
             case Theme.themeLight.name:
                 return Theme.themeLight;
+            case Theme.themeMulledWine.name:
+                return Theme.themeMulledWine;
+            case Theme.themeOcean.name:
+                return Theme.themeOcean;
+            case Theme.themePaleNight.name:
+                return Theme.themePaleNight;
             default:
                 console.error(`invalid theme requested: ${name}`);
         }
