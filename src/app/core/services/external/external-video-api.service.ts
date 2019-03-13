425bfc8e14a4d2bf5a99c80edc000520c791980e
Mike Morano <mmorano@mikeandwan.us>
2019-03-13 09:00:38 -0400

    initial implementation of external video service

--- src/app/core/services/external/external-video-api.service.ts
+++ src/app/core/services/external/external-video-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { VideoCategory } from '../../models/video-category.model';
 import { Video } from '../../models/video.model';
@@ -17,30 +18,64 @@ export class ExternalVideoApiService implements VideoApiService {
     ) { }
 
     getCategories(): Observable<VideoCategory[]> {
-        throw new Error('Method not implemented.');
+        const url = this.getAbsoluteUrl(`video-categories`);
+
+        return this._http
+            .get<VideoCategory[]>(url);
     }
 
     getCategory(categoryId: number): Observable<VideoCategory> {
-        throw new Error('Method not implemented.');
+        const url = this.getAbsoluteUrl(`video-categories/${categoryId}`);
+
+        return this._http
+            .get<VideoCategory>(url);
     }
 
     getVideosByCategory(categoryId: number): Observable<Video[]> {
-        throw new Error('Method not implemented.');
+        const url = this.getAbsoluteUrl(`video-categories/${categoryId}/videos`);
+
+        return this._http
+            .get<Video[]>(url);
     }
 
     getComments(videoId: number): Observable<Comment[]> {
-        throw new Error('Method not implemented.');
+        const url = this.getAbsoluteUrl(`videos/${videoId}/comments`);
+
+        return this._http
+            .get<Comment[]>(url)
+            .pipe(
+                map(comments => {
+                    // deal with dates
+                    return comments.map((x: Comment) => {
+                        x.entryDate = new Date(x.entryDate.toString());
+                        return x;
+                    });
+                })
+            );
     }
 
     getRating(videoId: number): Observable<Rating> {
-        throw new Error('Method not implemented.');
+        const url = this.getAbsoluteUrl(`videos/${videoId}/rating`);
+
+        return this._http
+            .get<Rating>(url);
     }
 
     rateVideo(videoId: number, rating: number): Observable<number> {
-        throw new Error('Method not implemented.');
+        const url = this.getAbsoluteUrl(`videos/${videoId}/rating`);
+
+        return this._http
+            .patch<number>(url, { videoId: videoId, rating: rating });
     }
 
     addComment(videoId: number, comment: string): Observable<any> {
-        throw new Error('Method not implemented.');
+        const url = this.getAbsoluteUrl(`videos/${videoId}/comments`);
+
+        return this._http
+            .post(url, { videoId: videoId, comment: comment });
+    }
+
+    private getAbsoluteUrl(relativeUrl: string) {
+        return `${this._cfg.apiUrl}/${relativeUrl}`;
     }
 }
