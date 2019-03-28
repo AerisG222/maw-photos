a1faafe223058c66963107c83d314bcf1ddfed83
Mike Morano <mmorano@mikeandwan.us>
2019-03-28 18:47:48 -0400

    cleanup

--- src/app/help/help/help-info.model.ts
+++
@@ -1,3 +0,0 @@
-export interface HelpInfo {
-    version: string;
-}
--- src/app/help/help/help.component.ts
+++ src/app/help/help/help.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 
-import { HelpInfo } from './help-info.model';
-
 @Component({
     selector: 'app-help',
     templateUrl: './help.component.html',
     styleUrls: ['./help.component.scss']
 })
 export class HelpComponent {
-    info: HelpInfo = { version: '0.1.0' };
 
-    constructor() {
-
-    }
 }
