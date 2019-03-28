3d736b81de91e6f2780864925dca8a7707a5f24c
Mike Morano <mmorano@mikeandwan.us>
2019-03-27 21:41:48 -0400

    start to define layout for info panel in mobile/narrow view

--- src/app/mobile/mobile-view/mobile-view.component.ts
+++ src/app/mobile/mobile-view/mobile-view.component.ts
@@ -1,10 +1,36 @@
-import { Component, Input, TemplateRef } from '@angular/core';
+import { Component, Input, TemplateRef, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+import { RootStoreState, LayoutStoreSelectors } from 'src/app/core/root-store';
 
 @Component({
   selector: 'app-mobile-view',
   templateUrl: './mobile-view.component.html',
   styleUrls: ['./mobile-view.component.scss']
 })
-export class MobileViewComponent {
+export class MobileViewComponent implements OnInit {
     @Input() routerTemplate: TemplateRef<any>;
+
+    isRightSidebarDisplayed$: Observable<boolean>;
+    hidePanels$: Observable<boolean>;
+
+    constructor(
+        private _store$: Store<RootStoreState.State>
+    ) { }
+
+    ngOnInit() {
+        this.isRightSidebarDisplayed$ = this._store$
+            .pipe(
+                select(LayoutStoreSelectors.selectLayoutIsRightSidebarDisplayed),
+                delay(0)
+            );
+
+        this.hidePanels$ = this._store$
+            .pipe(
+                select(LayoutStoreSelectors.selectLayoutIsFullscreen),
+                delay(0)
+            );
+    }
 }
