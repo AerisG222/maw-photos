0bb698d4cfc0d3f793a18b09e7ed20a224e110e8
Mike Morano <mmorano@mikeandwan.us>
2019-10-11 19:24:04 -0400

    use style (rather than css classes) to set the height/width for lazy loaded images for perf

--- src/app/categories/video-list/video-list.component.ts
+++ src/app/categories/video-list/video-list.component.ts
@@ -10,10 +10,45 @@ import { ThumbnailSize } from 'src/app/core/models/thumbnail-size.model';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class VideoListComponent {
+    _thumbnailSize: ThumbnailSize;
+    imgWidth: number;
+    imgHeight: number;
+
     @Input() videos: Video[];
     @Input() selectedVideo: Video;
-    @Input() thumbnailSize: ThumbnailSize;
     @Output() videoSelected = new EventEmitter<Video>();
+
+    get thumbnailSize() {
+        return this._thumbnailSize;
+    }
+
+    @Input()
+    set thumbnailSize(size: ThumbnailSize) {
+        this._thumbnailSize = size;
+
+        switch(size) {
+            case ThumbnailSize.verySmall:
+                this.imgWidth = 80;
+                this.imgHeight = 60;
+                break;
+            case ThumbnailSize.small:
+                this.imgWidth = 120;
+                this.imgHeight = 90;
+                break;
+            case ThumbnailSize.tiny:
+                this.imgWidth = 40;
+                this.imgHeight = 30;
+                break;
+            case ThumbnailSize.default:
+                this.imgWidth = 160;
+                this.imgHeight = 120;
+                break;
+            default:
+                this.imgWidth = 160;
+                this.imgHeight = 120;
+                break;
+        }
+    }
 
     onClickVideo(video: Video): void {
         this.videoSelected.emit(video);
--- src/app/shared/photo-list/photo-list.component.ts
+++ src/app/shared/photo-list/photo-list.component.ts
@@ -10,10 +10,45 @@ import { ThumbnailSize } from 'src/app/core/models/thumbnail-size.model';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PhotoListComponent {
+    _thumbnailSize: ThumbnailSize;
+    imgWidth: number;
+    imgHeight: number;
+
     @Input() photos: Photo[];
     @Input() selectedPhoto: Photo;
-    @Input() thumbnailSize: ThumbnailSize;
     @Output() photoSelected = new EventEmitter<Photo>();
+
+    get thumbnailSize() {
+        return this._thumbnailSize;
+    }
+
+    @Input()
+    set thumbnailSize(size: ThumbnailSize) {
+        this._thumbnailSize = size;
+
+        switch(size) {
+            case ThumbnailSize.verySmall:
+                this.imgWidth = 80;
+                this.imgHeight = 60;
+                break;
+            case ThumbnailSize.small:
+                this.imgWidth = 120;
+                this.imgHeight = 90;
+                break;
+            case ThumbnailSize.tiny:
+                this.imgWidth = 40;
+                this.imgHeight = 30;
+                break;
+            case ThumbnailSize.default:
+                this.imgWidth = 160;
+                this.imgHeight = 120;
+                break;
+            default:
+                this.imgWidth = 160;
+                this.imgHeight = 120;
+                break;
+        }
+    }
 
     onClickPhoto(photo: Photo): void {
         this.photoSelected.emit(photo);
