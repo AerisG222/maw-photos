cab465e2274ebca3c49cfdd96ba597c3f1ff6242
Mike Morano <mmorano@mikeandwan.us>
2020-01-26 13:23:51 -0500

    start to implement mock search api

--- src/app/core/services/mock/mock-search-api.service.ts
+++ src/app/core/services/mock/mock-search-api.service.ts
@@ -1,19 +1,92 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, Inject } from '@angular/core';
+import { Observable, forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { SearchApiService } from '../search-api.service';
 import { SearchResult } from '../../models/search/search-result.model';
 import { MultimediaCategory } from '../../models/search/multimedia-category.model';
+import { photoApiServiceToken, PhotoApiService } from '../photo-api.service';
+import { videoApiServiceToken, VideoApiService } from '../video-api.service';
 
 @Injectable()
 export class MockSearchApiService implements SearchApiService {
-    constructor() { }
+    constructor(
+        @Inject(photoApiServiceToken) private photoApi: PhotoApiService,
+        @Inject(videoApiServiceToken) private videoApi: VideoApiService
+    ) {
+
+    }
 
     search(query: string): Observable<SearchResult<MultimediaCategory>> {
-        throw new Error('Method not implemented.');
+        return this.getAll().pipe(
+            map(c => ({
+                totalFound: 20,
+                startIndex: 0,
+                results: c
+            })));
     }
 
     searchNextPage(start: number): Observable<SearchResult<MultimediaCategory>> {
-        throw new Error('Method not implemented.');
+        return this.search('abc').pipe(
+            map(r => ({
+                ...r,
+                startIndex: start
+            }))
+        );
+    }
+
+    private getAll() {
+        const photoCategories = this.photoApi.getCategories().pipe(
+            map(cats => {
+                const list: MultimediaCategory[] = [];
+
+                cats.items.forEach(c =>
+                    list.push(({
+                        solrId: `photo_${ c.id }`,
+                        id: c.id,
+                        year: c.year,
+                        name: c.name,
+                        multimediaType: 'photo',
+                        teaserPhotoHeight: c.teaserImage.height,
+                        teaserPhotoWidth: c.teaserImage.width,
+                        teaserPhotoPath: c.teaserImage.url,
+                        teaserPhotoSqHeight: c.teaserImageSq.height,
+                        teaserPhotoSqWidth: c.teaserImageSq.width,
+                        teaserPhotoSqPath: c.teaserImageSq.url
+                    }))
+                );
+
+                return list;
+            }));
+
+        const videoCategories = this.videoApi.getCategories().pipe(
+            map(cats => {
+                const list: MultimediaCategory[] = [];
+
+                cats.items.forEach(c =>
+                    list.push(({
+                        solrId: `video_${ c.id }`,
+                        id: c.id,
+                        year: c.year,
+                        name: c.name,
+                        multimediaType: 'video',
+                        teaserPhotoHeight: c.teaserImage.height,
+                        teaserPhotoWidth: c.teaserImage.width,
+                        teaserPhotoPath: c.teaserImage.url,
+                        teaserPhotoSqHeight: c.teaserImageSq.height,
+                        teaserPhotoSqWidth: c.teaserImageSq.width,
+                        teaserPhotoSqPath: c.teaserImageSq.url
+                    }))
+                );
+
+                return list;
+            }));
+
+        return forkJoin([
+                photoCategories,
+                videoCategories
+            ]).pipe(
+                map(([photoCats, videoCats]) => [ ...photoCats, ...videoCats ])
+            );
     }
 }
