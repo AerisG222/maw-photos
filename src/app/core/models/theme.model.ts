4ec6409577a92ab8e7dcde0362c3e0ee3b89c456
Mike Morano <mmorano@mikeandwan.us>
2020-02-05 20:23:02 -0500

    kill some of the uglier themes, pale night, youre on deck

--- src/app/core/models/theme.model.ts
+++ src/app/core/models/theme.model.ts
@@ -1,17 +1,13 @@
 export class Theme {
     static readonly themeDark = new Theme('Dark', 'maw-dark-theme', true);
-    static readonly themeDeepBlue = new Theme('Deep Blue', 'maw-deep-blue-theme', true);
     static readonly themeLight = new Theme('Light', 'maw-light-theme', false);
     static readonly themeMulledWine = new Theme('Mulled Wine', 'maw-mulled-wine-theme', true);
-    static readonly themeOcean = new Theme('Ocean', 'maw-ocean-theme', true);
     static readonly themePaleNight = new Theme('Pale Night', 'maw-pale-night-theme', true);
 
     static readonly allThemes = [
         Theme.themeDark,
-        Theme.themeDeepBlue,
         Theme.themeLight,
         Theme.themeMulledWine,
-        Theme.themeOcean,
         Theme.themePaleNight
     ];
 
@@ -29,14 +25,10 @@ export class Theme {
         switch (name) {
             case Theme.themeDark.name:
                 return Theme.themeDark;
-            case Theme.themeDeepBlue.name:
-                return Theme.themeDeepBlue;
             case Theme.themeLight.name:
                 return Theme.themeLight;
             case Theme.themeMulledWine.name:
                 return Theme.themeMulledWine;
-            case Theme.themeOcean.name:
-                return Theme.themeOcean;
             case Theme.themePaleNight.name:
                 return Theme.themePaleNight;
             default:
