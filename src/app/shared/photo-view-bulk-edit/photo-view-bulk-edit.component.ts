da4ccec07447eea457620eb04b7a4d4cee6c62ec
Mike Morano <mmorano@mikeandwan.us>
2020-03-08 08:37:45 -0400

    wip

--- src/app/shared/photo-view-bulk-edit/photo-view-bulk-edit.component.ts
+++ src/app/shared/photo-view-bulk-edit/photo-view-bulk-edit.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { LayoutStoreActions } from 'src/app/core/root-store';
-import { Store } from '@ngrx/store';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs/internal/Observable';
+
+import { Category } from 'src/app/core/models/category.model';
+import { LayoutStoreActions, PhotoCategoryStoreSelectors, PhotoStoreSelectors } from 'src/app/core/root-store';
+import { Photo } from 'src/app/core/models/photo.model';
 
 @Component({
     selector: 'app-photo-view-bulk-edit',
     templateUrl: './photo-view-bulk-edit.component.html',
     styleUrls: ['./photo-view-bulk-edit.component.scss']
 })
-export class PhotoViewBulkEditComponent implements OnInit {
+export class PhotoViewBulkEditComponent implements OnInit, OnDestroy {
+    category$: Observable<Category>;
+    photos$: Observable<Photo[]>;
+    toggleSelectButtonText = 'Select All';
+
     constructor(
         private store$: Store<{}>
     ) {
@@ -15,10 +23,30 @@ export class PhotoViewBulkEditComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.category$ = this.store$
+            .pipe(
+                select(PhotoCategoryStoreSelectors.selectCurrentCategoryAsCategory)
+            );
 
+        this.photos$ = this.store$
+            .pipe(
+                select(PhotoStoreSelectors.selectAllPhotos)
+            );
     }
 
     ngOnDestroy(): void {
         this.store$.dispatch(LayoutStoreActions.openRightSidebarRequest());
+    }
+
+    onToggleSelectAll(): void {
+        if(this.toggleSelectButtonText === 'Select All') {
+            this.toggleSelectButtonText = 'Deselect All';
+        } else {
+            this.toggleSelectButtonText = 'Select All';
+        }
+    }
+
+    trackByPhoto(index: number, photo: Photo): string {
+        return !!photo ? null : photo.id.toString();
     }
 }
