d0e4e8b7ca98ca32f958c4366f9d9ffd1abdb477
Mike Morano <mmorano@mikeandwan.us>
2020-04-07 09:58:56 -0400

    introduce base module, and create a toolbar module

---
+++ src/app/base/base.module.ts
@@ -0,0 +1,79 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { NgxStarsModule } from 'ngx-stars';
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        GoogleMapsModule,
+        MatButtonModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatTableModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        NgxStarsModule,
+        ReactiveFormsModule,
+        RouterModule
+    ],
+    exports: [
+        CommonModule,
+        GoogleMapsModule,
+        MatButtonModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatTableModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        NgxStarsModule,
+        ReactiveFormsModule,
+        RouterModule
+    ]
+})
+export class BaseModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,27 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { GoogleMapsModule } from '@angular/google-maps';
-import { RouterModule } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatTableModule } from '@angular/material/table';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { NgxStarsModule } from 'ngx-stars';
 
 import { BulkEditSidebarComponent } from './bulk-edit-sidebar/bulk-edit-sidebar.component';
 import { CategoriesLinkComponent } from './categories-link/categories-link.component';
@@ -70,12 +47,6 @@ import { SidebarCardDividerComponent } from './sidebar-card-divider/sidebar-card
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SlideshowButtonComponent } from './slideshow-button/slideshow-button.component';
 import { StatsLinkComponent } from './stats-link/stats-link.component';
-import { ToolbarButtonComponent } from './toolbar-button/toolbar-button.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { ToolbarDividerComponent } from './toolbar-divider/toolbar-divider.component';
-import { ToolbarExternalLinkComponent } from './toolbar-external-link/toolbar-external-link.component';
-import { ToolbarGroupComponent } from './toolbar-group/toolbar-group.component';
-import { ToolbarLinkComponent } from './toolbar-link/toolbar-link.component';
 import { BulkEditFilterComponent } from './bulk-edit-filter/bulk-edit-filter.component';
 import { BulkEditGpsEditorComponent } from './bulk-edit-gps-editor/bulk-edit-gps-editor.component';
 import { SidebarButtonGroupComponent } from './sidebar-button-group/sidebar-button-group.component';
@@ -83,6 +54,8 @@ import { SidebarCardGroupComponent } from './sidebar-card-group/sidebar-card-gro
 import { SidebarButtonDividerComponent } from './sidebar-button-divider/sidebar-button-divider.component';
 import { PrimaryNavLinkComponent } from './primary-nav-link/primary-nav-link.component';
 import { ContentMarginComponent } from './content-margin/content-margin.component';
+import { BaseModule } from '../base/base.module';
+import { ToolbarModule } from './toolbar/toolbar.module';
 
 @NgModule({
     declarations: [
@@ -137,65 +110,17 @@ import { ContentMarginComponent } from './content-margin/content-margin.componen
         SidebarComponent,
         SlideshowButtonComponent,
         StatsLinkComponent,
-        ToolbarComponent,
-        ToolbarGroupComponent,
-        ToolbarButtonComponent,
-        ToolbarDividerComponent,
-        ToolbarExternalLinkComponent,
-        ToolbarLinkComponent,
         SidebarButtonGroupComponent,
         SidebarCardGroupComponent,
         SidebarButtonDividerComponent
     ],
     imports: [
-        CommonModule,
-        GoogleMapsModule,
-        MatButtonModule,
-        MatCardModule,
-        MatCheckboxModule,
-        MatDialogModule,
-        MatDividerModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule,
-        MatListModule,
-        MatProgressSpinnerModule,
-        MatRadioModule,
-        MatSelectModule,
-        MatSliderModule,
-        MatSlideToggleModule,
-        MatTableModule,
-        MatTabsModule,
-        MatToolbarModule,
-        MatTooltipModule,
-        NgxStarsModule,
-        ReactiveFormsModule,
-        RouterModule
+        BaseModule,
+        ToolbarModule
     ],
     exports: [
-        // modules
-        CommonModule,
-        GoogleMapsModule,
-        MatButtonModule,
-        MatCardModule,
-        MatCheckboxModule,
-        MatDialogModule,
-        MatDividerModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule,
-        MatListModule,
-        MatProgressSpinnerModule,
-        MatRadioModule,
-        MatSelectModule,
-        MatSliderModule,
-        MatSlideToggleModule,
-        MatTableModule,
-        MatTabsModule,
-        MatToolbarModule,
-        MatTooltipModule,
-        NgxStarsModule,
-        ReactiveFormsModule,
+        BaseModule,
+        ToolbarModule,
 
         // components
         BulkEditFilterComponent,
@@ -240,13 +165,7 @@ import { ContentMarginComponent } from './content-margin/content-margin.componen
         SidebarCardGroupComponent,
         SidebarCardDividerComponent,
         SidebarComponent,
-        SlideshowButtonComponent,
-        ToolbarComponent,
-        ToolbarButtonComponent,
-        ToolbarDividerComponent,
-        ToolbarExternalLinkComponent,
-        ToolbarGroupComponent,
-        ToolbarLinkComponent
+        SlideshowButtonComponent
     ]
 })
 export class SharedModule { }
