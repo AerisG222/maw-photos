0e1c8aeb148dfc4cb2caac02780c39b1fe9ca995
Mike Morano <mmorano@mikeandwan.us>
2020-05-13 19:23:48 -0400

    start to support container dev deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,10 @@ export const environment = {
     production: false,
     version: require('../../package.json').version,
 
-    apiUrl: 'https://apidev.mikeandwan.us:5011',
-    authUrl: 'https://authdev.mikeandwan.us:5001',
-    photosUrl: 'http://photosdev.mikeandwan.us:4200',
-    wwwUrl: 'https://wwwdev.mikeandwan.us:5021'
+    apiUrl: 'https://apidev.mikeandwan.us',
+    authUrl: 'https://authdev.mikeandwan.us',
+    photosUrl: 'http://photosdev.mikeandwan.us',
+    wwwUrl: 'https://wwwdev.mikeandwan.us'
 };
 
 /*
