6aca29d3534acb92654b591e65daac0ee68b3475
Mike Morano <mmorano@mikeandwan.us>
2020-05-31 12:16:08 -0400

    update libs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,17 @@ export const environment = {
     production: false,
     version: require('../../package.json').version,
 
-    apiUrl: 'https://apidev.mikeandwan.us',
-    authUrl: 'https://authdev.mikeandwan.us',
-    photosUrl: 'http://photosdev.mikeandwan.us',
-    wwwUrl: 'https://wwwdev.mikeandwan.us'
+    // local dev
+    apiUrl: 'https://apidev.mikeandwan.us:5011',
+    authUrl: 'https://authdev.mikeandwan.us:5001',
+    photosUrl: 'http://photosdev.mikeandwan.us:4200',
+    wwwUrl: 'https://wwwdev.mikeandwan.us:5021'
+
+    // container dev
+    // apiUrl: 'https://apidev.mikeandwan.us',
+    // authUrl: 'https://authdev.mikeandwan.us',
+    // photosUrl: 'http://photosdev.mikeandwan.us',
+    // wwwUrl: 'https://wwwdev.mikeandwan.us'
 };
 
 /*
