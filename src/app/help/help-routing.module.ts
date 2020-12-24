12a93c16edcfb6c63fb3b1cb55b0fcf222a68cc0
Mike Morano <mmorano@mikeandwan.us>
2020-12-24 14:07:00 -0500

    break help into separate components

--- src/app/help/help-routing.module.ts
+++ src/app/help/help-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AndroidComponent } from './android/android.component';
+import { HelpDetailsComponent } from './help-details/help-details.component';
 
 import { HelpComponent } from './help/help.component';
+import { ReleaseNotesComponent } from './release-notes/release-notes.component';
 
 const routes: Routes = [
-    { path: '', component: HelpComponent },
-    { path: '**', redirectTo: '' }
+    { path: '', component: HelpComponent,
+        children: [
+            { path: 'help', component: HelpDetailsComponent },
+            { path: 'release-notes', component: ReleaseNotesComponent },
+            { path: 'android', component: AndroidComponent },
+            { path: '', redirectTo: 'help', pathMatch: 'full' }
+        ]
+    },
+    { path: '**', redirectTo: './help/help' }
 ];
 
 @NgModule({
--- src/app/help/help.module.ts
+++ src/app/help/help.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HelpRoutingModule } from './help-routing.module';
 import { HelpComponent } from './help/help.component';
+import { HelpDetailsComponent } from './help-details/help-details.component';
+import { ReleaseNotesComponent } from './release-notes/release-notes.component';
+import { AndroidComponent } from './android/android.component';
 
 @NgModule({
     declarations: [
-        HelpComponent
+        HelpComponent,
+        HelpDetailsComponent,
+        ReleaseNotesComponent,
+        AndroidComponent
     ],
     imports: [
         HelpRoutingModule,
