e4630e00585176d00c8792619b2b8da2c4bc0a15
Mike Morano <mmorano@mikeandwan.us>
2021-01-02 21:37:21 -0500

    export photos store for consistency

--- src/app/core/root-store/index.ts
+++ src/app/core/root-store/index.ts
@@ -4,6 +4,7 @@ import * as RootStoreSelectors from './selectors';
 export * from './auth-store';
 export * from './layout-store';
 export * from './photo-category-store';
+export * from './photos-store';
 export * from './router-store';
 export * from './settings-store';
 export * from './video-category-store';
