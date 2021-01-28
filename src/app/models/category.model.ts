712a840b145442919d70301f968c7eea7788995b
Mike Morano <mmorano@mikeandwan.us>
2021-01-28 07:47:38 -0500

    DRY

--- src/app/models/category.model.ts
+++ src/app/models/category.model.ts
@@ -1,17 +1,8 @@
-import { CategoryType } from './category-type.model';
-import { MultimediaAsset } from './api/multimedia-asset.model';
 import { PhotoCategory } from './api/photo-category.model';
 import { VideoCategory } from './api/video-category.model';
 import { CategoryTeaser } from './category-teaser.model';
 
-export interface Category extends CategoryTeaser{
-    route: string;
-    id: number;
-    year: number;
-    name: string;
-    type: CategoryType;
+export interface Category extends CategoryTeaser {
     createDate: Date;
-    teaserImage: MultimediaAsset;
-    teaserImageSq: MultimediaAsset;
     actual: PhotoCategory | VideoCategory;
 }
