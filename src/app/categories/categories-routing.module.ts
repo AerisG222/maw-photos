7a0a2b9257e8ec0e12b4c1001db9951813df1d9f
Mike Morano <mmorano@mikeandwan.us>
2021-01-30 20:09:36 -0500

    cleanup

--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { YearListComponent } from './components/year-list/year-list.component';
-import { RouteHelper } from '@models';
 import { ViewModeGuard } from './services/view-mode.guard';
 
 const routes: Routes = [
