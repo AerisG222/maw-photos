8c2ed44b99c2d726847fd6dc1b8403dbbdef22bb
Mike Morano <mmorano@mikeandwan.us>
2021-02-10 21:18:35 -0500

    initial cut at search settings form

--- src/app/settings/search-settings/search-settings.component.ts
+++ src/app/settings/search-settings/search-settings.component.ts
@@ -1,9 +1,15 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { SearchGridSettingsFacade } from '@core/facades/settings/search-grid-settings-facade';
+import { SearchListSettingsFacade } from '@core/facades/settings/search-list-settings-facade';
+import { SearchPageSettingsFacade } from '@core/facades/settings/search-page-settings-facade';
 
 import { allCategoryViewModes, CategoryMargin, ThumbnailSize } from '@models';
-import { DEFAULT_SEARCH_GRID_VIEW_SETTINGS } from 'src/app/models/settings/search-grid-view-settings';
-import { DEFAULT_SEARCH_SETTINGS } from 'src/app/models/settings/search-page-settings';
+import { combineLatest } from 'rxjs';
+import { first } from 'rxjs/operators';
+import { SearchGridViewSettings } from 'src/app/models/settings/search-grid-view-settings';
+import { SearchListViewSettings } from 'src/app/models/settings/search-list-view-settings';
+import { SearchPageSettings } from 'src/app/models/settings/search-page-settings';
 
 @Component({
     selector: 'app-search-settings',
@@ -17,29 +23,96 @@ export class SearchSettingsComponent {
     viewModes = allCategoryViewModes;
     thumbnailSizes = ThumbnailSize.allSizes;
 
-    constructor(private fb: FormBuilder) {
+    constructor(
+        private fb: FormBuilder,
+        private gridFacade: SearchGridSettingsFacade,
+        private listFacade: SearchListSettingsFacade,
+        private pageFacade: SearchPageSettingsFacade
+    ) {
         this.form = this.fb.group({
             page: this.fb.group({
-                viewMode: DEFAULT_SEARCH_SETTINGS.viewMode
+                viewMode: ''
             }),
             grid: this.fb.group({
-                showTitles: DEFAULT_SEARCH_GRID_VIEW_SETTINGS.showTitles,
-                showYears: DEFAULT_SEARCH_GRID_VIEW_SETTINGS.showYears,
-                margin: DEFAULT_SEARCH_GRID_VIEW_SETTINGS.margin,
-                thumbnailSize: DEFAULT_SEARCH_GRID_VIEW_SETTINGS.thumbnailSize
+                showTitles: '',
+                showYears: '',
+                margin: '',
+                thumbnailSize: ''
             }),
             list: this.fb.group({
-                margin: DEFAULT_SEARCH_GRID_VIEW_SETTINGS.margin,
-                thumbnailSize: DEFAULT_SEARCH_GRID_VIEW_SETTINGS.thumbnailSize
+                margin: '',
+                thumbnailSize: ''
             })
         });
+
+        this.resetForm();
     }
 
     onSave() {
+        if(!this.form.valid) {
+            return;
+        }
 
+        const grid = this.readGridForm();
+        const list = this.readListForm();
+        const page = this.readPageForm();
+
+        this.gridFacade.save(grid);
+        this.listFacade.save(list);
+        this.pageFacade.save(page);
     }
 
     onCancel() {
+        this.resetForm();
+    }
 
+    private readGridForm(): SearchGridViewSettings {
+        return {
+            margin: CategoryMargin.forName(this.form.get('grid.margin')?.value),
+            showTitles: this.form.get('grid.showTitles')?.value,
+            showYears: this.form.get('grid.showYears')?.value,
+            thumbnailSize: ThumbnailSize.forName(this.form.get('grid.thumbnailSize')?.value)
+        };
+    }
+
+    private readListForm(): SearchListViewSettings {
+        return {
+            margin: CategoryMargin.forName(this.form.get('list.margin')?.value),
+            thumbnailSize: ThumbnailSize.forName(this.form.get('list.thumbnailSize')?.value)
+        };
+    }
+
+    private readPageForm(): SearchPageSettings {
+        return {
+            viewMode: this.form.get('list.thumbnailSize')?.value
+        };
+    }
+
+    private resetForm() {
+        combineLatest([
+            this.gridFacade.settings$,
+            this.listFacade.settings$,
+            this.pageFacade.settings$
+        ]).pipe(
+            first()
+        ).subscribe({
+            next: ([grid, list, page]) => {
+                return {
+                    page: {
+                        viewMode: page.viewMode
+                    },
+                    grid: {
+                        showTitles: grid.showTitles,
+                        showYears: grid.showYears,
+                        margin: grid.margin.name,
+                        thumbnailSize: grid.thumbnailSize.name
+                    },
+                    list: {
+                        margin: list.margin.name,
+                        thumbnailSize: list.thumbnailSize.name
+                    }
+                };
+            }
+        });
     }
 }
