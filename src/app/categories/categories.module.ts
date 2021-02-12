7f73d87c5b52f5a9eb0d489002a6ebf9936e7bcd
Mike Morano <mmorano@mikeandwan.us>
2021-02-12 16:53:33 -0500

    remove deleted items

--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { CategoriesRoutingModule } from './categories-routing.module';
 
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { CategoryTypeFilterComponent } from './components/category-type-filter/category-type-filter.component';
 import { CategoryYearFilterComponent } from './components/category-year-filter/category-year-filter.component';
 import { CategoryMissingGpsFilterComponent } from './components/category-missing-gps-filter/category-missing-gps-filter.component';
 import { YearComponent } from './components/year/year.component';
-import { YearListComponent } from './components/year-list/year-list.component';
 import { CategoriesStoreModule } from './store/categories-store.module';
 import { CategoriesUrlService } from './services/categories-url.service';
 import { ViewModeGuard } from './services/view-mode.guard';
@@ -21,11 +19,9 @@ import { ListViewComponent } from './components/list-view/list-view.component';
 
 @NgModule({
     declarations: [
-        ToolbarComponent,
         CategoryTypeFilterComponent,
         CategoryYearFilterComponent,
         YearComponent,
-        YearListComponent,
         CategoryMissingGpsFilterComponent,
         ToolbarGroupSelectViewComponent,
         GridToolbarComponent,
