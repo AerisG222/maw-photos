dc95823a16b14655fb596ee316c8022fad9192b1
Mike Morano <mmorano@mikeandwan.us>
2021-02-17 11:51:10 -0500

    start to refine lint settings

--- src/app/search/components/no-results-found/no-results-found.component.ts
+++ src/app/search/components/no-results-found/no-results-found.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-no-results-found',
@@ -6,11 +6,6 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./no-results-found.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NoResultsFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class NoResultsFoundComponent {
 
 }
