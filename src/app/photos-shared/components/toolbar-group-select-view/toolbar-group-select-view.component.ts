a12040b96d2e34cf7e711ad60c89623a3d7c7066
Mike Morano <mmorano@mikeandwan.us>
2021-02-22 22:03:40 -0500

    remove props that are no longer needed

--- src/app/photos-shared/components/toolbar-group-select-view/toolbar-group-select-view.component.ts
+++ src/app/photos-shared/components/toolbar-group-select-view/toolbar-group-select-view.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterStoreSelectors } from '@core/root-store';
 import { Store } from '@ngrx/store';
 
@@ -9,13 +9,6 @@ import { Store } from '@ngrx/store';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToolbarGroupSelectViewComponent {
-    // TODO: remove these inputs
-    @Input() isBulkEditViewActive = false;
-    @Input() isDetailViewActive = false;
-    @Input() isFullscreenViewActive = false;
-    @Input() isGridViewActive = false;
-    @Input() isMapViewActive = false;
-
     enableMapView$ = this.store.select(RouterStoreSelectors.inPhotosArea);
     enableBulkEdit$ = this.store.select(RouterStoreSelectors.inPhotosArea);
 
